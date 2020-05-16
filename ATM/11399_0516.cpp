0e354aa6fdc77b9c8face447d7899056a046a7e8
yu-ri-kim <yu-ri-@naver.com>
2020-05-16 12:00:43 +0900

    20200616

--- ATM/11399_0516.cpp
+++ ATM/11399_0516.cpp
@@ -15,14 +15,14 @@ int main(){
         datas.push_back(tmp);
     }
 
-    sort(datas.begin(), datas.end());
+    sort(datas.begin(), datas.end()); // 가장 작은 순으로 더해야 함
 
     dp[0]= datas[0];
     int sum = dp[0];
 
     for(int i=1;i<n;i++){
-        dp[i] = dp[i-1] + datas[i];
-        sum += dp[i];
+        dp[i] = dp[i-1] + datas[i]; // i-1까지 걸린 시간 + i가 걸리는 시간
+        sum += dp[i]; // 최종 시간은 걸린 시간들의 합
     }
     
     cout << sum;
